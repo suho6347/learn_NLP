d631b6b7237251ce5897578295410071af2f4bcc
Aritta <33124040+suho6347@users.noreply.github.com>
2023-07-21 08:51:10 +0900

    Create 01-01-list.py

---
+++ 01-Python/01-01-list.py
@@ -0,0 +1,39 @@
+"""
+파이썬에 대해 알아보자!
+
+숫자나 단어, 문장 등을 의미있게 집합시키기 위해
+파이썬에서는 list라는 자료형을 사용합니다.
+"""
+
+
+
+#홀수를 담은 list
+odd = [1, 3, 5, 7, 9]
+print(odd)
+
+
+
+
+#문자를 담은 list
+char = ['a', 'b', 'c']
+print(char)
+#Python에서는 'a'와 "a"를 다르지 않다고 인식합니다.
+#즉, '(따옴표)와 "(쌍따옴표) 중 어떤 것을 사용해도 괜찮습니다!
+
+
+
+
+#단어를 담은 list
+word = ['life', 'time', 'gold']
+print(word)
+
+
+
+
+#문장을 담은 list
+sent = ['life is too short', 'time is a gold']
+print(sent)
+
+
+
+
