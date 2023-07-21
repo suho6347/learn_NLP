a04ed7bb4506c08af229aa30d73af5bbb4b02901
Aritta <33124040+suho6347@users.noreply.github.com>
2023-07-21 11:31:11 +0900

    Update 01-03-numpy.py

--- 01-Python/01-03-numpy.py
+++ 01-Python/01-03-numpy.py
@@ -18,7 +18,9 @@ print(arr) # ['elephant' 'horse' 'monkey']
 
 # list가 못하는데 numpy array는 가능한 것
 # 즉, numpy array를 쓰는 이유
+# 참조 : https://doorbw.tistory.com/171
 
+# 초급 ☆
 # size, shape
 contry = [['Angola', 'Argentina', 'Austria', 'Australia'], ['Bangladesh', 'Belgium', 'Bolivia', 'Belarus']]
 contry_arr = np.array(contry)
@@ -29,6 +31,43 @@ print(contry_arr.shape) # (2, 4)
 # 그러나 numpy array의 'size'를 사용하면 2x4 행렬의 모든 원소의 수를 계산해 줍니다.
 # 심지어 'shape'를 사용하면 행렬의 형태를 바로 알려줍니다!
 
-# example
-# 주로 NLP에서 사용되는 부분은 train 과정입니다.
-# 
+row, col = contry_arr.shape
+print(row) # 2
+print(col) # 4
+# 행과 열로 나누어 인식시킬 수도 있습니다.
+
+
+
+
+# 중급 ☆☆
+# 이 부분에서 사용되는 것이 어떻게 작동되는지 잘 이해하셔야
+# 실수 없는 Python 코딩이 완성됩니다.
+
+# axis
+arr1 = np.random.randn(5, 3) # 5x3 행렬을 만들어주세요, 안에 있는 값은 정규분포값(randn)에 따라 랜덤으로 설정합니다.
+print(arr1)
+
+print(np.sum(arr1, axis=0)) # 5x3에서 '5'부분을 압축시켜 합쳐주세요. 결과는 1x3 행렬이 됩니다.
+print(np.sum(arr1, axis=1)) # 5x3에서 '3'부분을 압축시켜 합쳐주세요. 결과는 1x5 행렬이 됩니다.
+
+print(np.sort(arr1, axis=0)) # 5x3에서 '5'부분을 기준으로 5,5,5 세로로 정렬해주세요.
+print(np.sort(arr1, axis=1)) # 5x3에서 '3'부분을 기준으로 3,3,3,3,3 가로로 정렬해주세요.
+
+
+
+
+# 고급 ☆☆☆
+# 이 부분을 잘 이해하게 되면 
+# 난이도있는 코드들을 읽을 수 있게 됩니다.
+
+# masking
+arr2 = np.random.randn(5,3)
+print(arr2)
+
+arr2_mask = arr2 > 0.5 # arr2에서 0.5이상의 값들 위치만 True인 새로운 list를 만듭니다.
+print(arr2_mask)
+
+print(arr2[arr2_mask]) # True였던 값들, 즉, 여기서는 0.5 이상인 값들을 따로 뺄 수 있습니다.
+
+arr2[arr2_mask] = 0 # 또는 True였던 부분들을 원하는 값(여기서는 0)으로 덮어씌울 수 있습니다.
+print(arr2) 
